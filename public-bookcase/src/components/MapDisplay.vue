edf6c7fea64db85006e6c95b85d6289fec64e868
srg404 <srg404@gmail.com>
2021-05-18 12:24:58 +0200

    MapDisplay Done

---
+++ public-bookcase/src/components/MapDisplay.vue
@@ -0,0 +1,48 @@
+<template>
+    <l-map :zoom="zoom" :center="center">
+      <l-tile-layer :url="url"></l-tile-layer>
+      <l-marker
+        v-for="(item, index) in getList"
+        v-on:click="triggerPin(item)"
+        :key="index"
+        :icon="icon"
+        :lat-lng="[item.coordinates[1],item.coordinates[0]]"
+        :name="item.name"
+      />
+    </l-map>
+</template>
+
+<script>
+import L from 'leaflet';
+import { mapGetters } from 'vuex';
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import pinImg from '../assets/images/pin.svg';
+
+export default {
+  components: {
+    LMap,
+    LMarker,
+    LTileLayer,
+  },
+  data() {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      zoom: 14,
+      center: [50.489, 5.095],
+      icon: L.icon({
+        iconUrl: pinImg,
+        iconSize: [32, 40],
+        iconAnchor: [16, 40],
+      }),
+    };
+  },
+  computed: {
+    ...mapGetters(['getList']),
+  },
+  methods: {
+    triggerPin: function triggerPin(value) {
+      this.$emit('openModal', value);
+    },
+  },
+};
+</script>
--- public-bookcase/src/views/Map.vue
+++ public-bookcase/src/views/Map.vue
@@ -1,74 +1,42 @@
 <template>
   <div class="map">
-    <l-map :zoom="zoom" :center="center">
-    <l-tile-layer :url="url"></l-tile-layer>
-    <l-marker
-      v-for="(item, index) in circles"
-      v-on:click="doClick"
-      :key="index"
-      :icon="icon"
-      :lat-lng="item.center"
-      :options="item.options"
-    />
-    </l-map>
-
+    <MapDisplay v-on:openModal="openModal"/>
   </div>
 </template>
 
 <script>
-import L from 'leaflet';
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
-import pinImg from '../assets/images/pin.svg';
+import MapDisplay from '@/components/MapDisplay.vue';
 
 export default {
   components: {
-    LMap,
-    LMarker,
-    LTileLayer,
+    MapDisplay,
   },
   data() {
     return {
-      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      zoom: 14,
-      center: [50.489, 5.095],
-      icon: L.icon({
-        iconUrl: pinImg,
-        iconSize: [32, 40],
-        iconAnchor: [16, 40],
-      }),
-      circles: [{
-        center: [50.489, 5.095],
-        options: {
-          name: 'rue truc muche',
-          id: '786513213',
-        },
-      },
-      {
-        center: [50.488, 5.097],
-        options: {
-          name: 'rue truc',
-          id: 'aaaaaaaaaaa786513213',
-        },
-      }],
+      isOpen: false,
+      currentBookcase: {},
     };
   },
   methods: {
-    doClick: function triggerButton(value) {
-      // console.log('v -> ', value);
-      console.log('v -> ', value.sourceTarget.options.id);
-      console.log('v -> ', value.sourceTarget.options.name);
+    openModal: function openModal(value) {
+      this.currentBookcase = value;
+      this.isOpen = true;
+      console.log('Open Modal data -> ', value);
+    },
+    closeModal: function closeModal() {
+      this.currentBookcase = {};
+      this.isOpen = false;
+      console.log('Close Modal');
     },
   },
 };
-</script>
 
+</script>
 <style scoped lang="scss">
   .map{
     position: relative;
     z-index: 1;
     height: 100vh;
     width: 100vw;
-    margin: auto;
-    border: 2px solid red;
   }
 </style>
